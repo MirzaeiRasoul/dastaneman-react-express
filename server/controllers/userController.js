a2ddfaecb8238d5009f64c15d3863c41066382ad
Rasoul Mirzaei Zadeh <mirzaei.rasoul172@gmail.com>
2024-07-20 20:15:41 +0330

    remove password from result

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,7 +3,7 @@ const User = require("../models/User")
 //** ======================== Get all users ========================
 const getAllUsers = async (req, res) => {
   try {
-    const users = await User.find()
+    const users = await User.find().select("-password")
     res.json({ success: true, result: users, error: null })
   } catch (err) {
     res.status(500).json({ success: false, result: null, error: { errorCode: 500, message: "An error occurred while fetching users: " + err.message } })
@@ -14,7 +14,7 @@ const getAllUsers = async (req, res) => {
 const getSingleUser = async (req, res) => {
   const userId = req.params.id
   try {
-    const user = await User.findById(userId)
+    const user = await User.findById(userId).select("-password")
     if (!user) return res.status(404).json({ success: false, result: null, error: { errorCode: 404, message: "User not Found!" } })
     res.json({ success: true, result: user, error: null })
   } catch (err) {
